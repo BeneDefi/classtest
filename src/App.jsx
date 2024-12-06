11a4e31ef89466de14cb58bc418702c642902bda
Benedict Maigida <teyeiesla@gmail.com>
2024-12-06 13:37:41 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import UserData from './UserData'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <UserData/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/UserData.jsx
@@ -0,0 +1,20 @@
+function UserData() {
+    return (
+      <>
+      <div className="container">
+      <div className="post">
+        <h2>Post</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam vel aliquam corporis magni voluptate optio cumque commodi qui nesciunt, laborum temporibus beatae fugit saepe enim odio id magnam harum vitae.</p>
+        <button className="bookmark">Bookmark</button>
+      </div>
+      <div className="userdata">
+      <h2>UserData</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam quo repellendus facilis quam ducimus natus hic aut fugit, consectetur sit error totam itaque eius ad nihil. Iusto repellendus natus placeat!</p>
+      <button className="follow">Follow</button>
+      </div>
+      </div>
+      </>
+    )
+  }
+  
+  export default UserData
